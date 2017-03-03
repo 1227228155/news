f347d0d9714b4811f8d5a3e2075fbc58b833a189
liuran <1227228155@qq.com>
2017-03-03 14:21:38 +0800

    first commit

---
+++ app/src/main/java/cn/ucai/news/ui/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.news.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ucai.news.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach);
+    }
+}
